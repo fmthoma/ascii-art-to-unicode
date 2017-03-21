d5c5e62b57e1acebaa8a84d9517d3a8f4192e85c
Franz Thoma <franz.thoma@tngtech.com>
2017-03-21 20:24:36 +0100

    Fix -i (inplace): Use strict IO

--- app/Main.hs
+++ app/Main.hs
@@ -3,24 +3,41 @@ module Main where
 
 import           Control.Applicative
 import           System.Environment
+import qualified System.IO.Strict as Strict
 import           Text.AsciiArt
+
+data Mode
+    = Inplace FilePath
+    | Read FilePath
+    | Pipe
 
 main :: IO ()
 main = do
-    (input, output) <- getArgs >>= \case
-        []           -> liftA2 (,) getContents (pure putStr)
-        ["-i", file] -> liftA2 (,) (readFile file) (pure (writeFile file))
-        [file]       -> liftA2 (,) (readFile file) (pure putStr)
+    mode <- getArgs <&> \case
+        []           -> Pipe
+        ["-i", file] -> Inplace file
+        [file]       -> Read file
+    input <- case mode of
+        Inplace file -> Strict.readFile file
+        Read    file -> readFile file
+        Pipe         -> getContents
     let inputLines = lines input
         plane = planeFromList ' ' inputLines
         width = maximum (fmap length inputLines)
         height = length inputLines
-    output
-        . unlines
-        . fmap trimRight
-        . planeToList height width
-        . renderAsciiToUnicode
-        $ plane
+    let unicodeArt
+            = unlines
+            . fmap trimRight
+            . planeToList height width
+            . renderAsciiToUnicode
+            $ plane
+    case mode of
+        Inplace file -> writeFile file unicodeArt
+        Read    _    -> putStr unicodeArt
+        Pipe         -> putStr unicodeArt
 
 trimRight :: String -> String
 trimRight = reverse . dropWhile (== ' ') . reverse
+
+(<&>) :: Functor f => f a -> (a -> b) -> f b
+(<&>) = flip (<$>)
